765232c7f7a6f956e2a08ea51b0ec5467fd32fb3
Etty <beltrami.ester@gmail.com>
2019-09-02 13:26:05 +0200

    Revert "Fix image type"
    This reverts commit 2732ca55e3c7ec75bb440449f0ea5475f6c589b0.

--- backend/api/users/types.py
+++ backend/api/users/types.py
@@ -26,6 +26,7 @@ class MeUser:
     address: Optional[str]
     country: str
     phone_number: Optional[str]
+    image: Optional[Image]
 
     @strawberry.field
     def tickets(self, info, conference: str) -> List["Ticket"]:
